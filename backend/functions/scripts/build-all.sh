b23428972e0764ee7f4640473cbd03ea7712d808
Kay <kay.knoepfle@exxeta.com>
2025-11-22 15:21:12 +0100

    remove not needed files

--- backend/functions/scripts/build-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-echo "🔨 Building all projects..."
-npm run build -w shared
-for dir in place-search image-classifier llm-analyzer data-persistence; do
-  echo "Building $dir..."
-  npm run build -w $dir
-done
