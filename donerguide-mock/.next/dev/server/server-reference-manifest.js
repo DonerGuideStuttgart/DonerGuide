4dd7e0e8fe51f7d77963028a0178a09c1cfa684c
Levartar <jakob.guenster@gmx.net>
2025-11-27 18:14:19 +0100

    delete mock data as backend mock already exists

--- donerguide-mock/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/ek6Sxz59HyAPotDJ3emCpUks9I6rH/MMGv0FVDQEbE=\"\n}"
--- donerguide-mock/.next/dev/static/chunks/ecedf_next_dist_client_components_builtin_layout_ded3991b.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/ecedf_next_dist_6244e8d5._.js"
-],
-    source: "dynamic"
-});
