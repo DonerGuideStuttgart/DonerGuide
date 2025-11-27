6961dcd2e02363caa4c4a24cb56de49b61496731
Levartar <jakob.guenster@gmx.net>
2025-11-27 12:48:44 +0100

    add basic store and dashboard pages as basic file structure

---
+++ frontend/src/app/stores/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <header>
+            <h1>Döner Stores Liste</h1>
+            <button className="btn btn-primary">Test</button>
+        </header>
+    )
+}
