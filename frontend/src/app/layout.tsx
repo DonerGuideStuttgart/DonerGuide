d57238d1f1f3beaf1b8f5239555e6486a2eb8bb2
anna_rinck <annarinck@gmx.de>
2025-11-30 23:12:52 +0100

    replace navbar with responsive daisyUI component.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,35 +1,35 @@
-import type { Metadata } from 'next'
-import { Rubik } from 'next/font/google'
+import type {Metadata} from 'next'
+import {Rubik} from 'next/font/google'
 import './globals.css'
 
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 const rubikSans = Rubik({
-	variable: '--font-rubik-sans',
-	subsets: ['latin'],
+    variable: '--font-rubik-sans',
+    subsets: ['latin'],
 })
 
 export const metadata: Metadata = {
-	title: {
-		template: '%s | Dönerguide Stuttgart',
-		default: 'Dönerguide Stuttgart',
-	},
-	description: 'Eine Web-App, um den besten Döner in Stuttgart zu finden.',
+    title: {
+        template: '%s | Dönerguide Stuttgart',
+        default: 'Dönerguide Stuttgart',
+    },
+    description: 'Eine Web-App, um den besten Döner in Stuttgart zu finden.',
 }
 
 export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode
 }>) {
-	return (
-		<html lang="de">
-			<body className={`${rubikSans.className} antialiased`}>
-				{/* <Navbar /> */}
-				{children}
-				<Footer />
-			</body>
-		</html>
-	)
+    return (
+        <html lang="de">
+        <body className={`${rubikSans.className} antialiased`}>
+        <nav className={"pb-16"}><Navbar/></nav>
+        {children}
+        <Footer/>
+        </body>
+        </html>
+    )
 }
