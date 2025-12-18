f02ef2f80ca578dd810c30876f137d69bc2d066b
Levartar <jakob.guenster@gmx.net>
2025-12-18 18:10:19 +0100

    add icons helper

---
+++ frontend/src/helpers/icons.tsx
@@ -0,0 +1,21 @@
+export const CloseIcon = () => {
+	return (
+		<path
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth={2}
+			d="M6 18L18 6M6 6l12 12"
+		/>
+	)
+}
+
+export const FilterMenuIcon = () => {
+	return (
+		<path
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth={2}
+			d="M4 21L4 14M4 10L4 3M12 21L12 12M12 8L12 3M20 21L20 16M20 12L20 3M1 14L7 14M9 8L15 8M17 16L23 16"
+		/>
+	)
+}
