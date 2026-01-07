d51cdf948812b1aac18b77f584b90901a05e32ec
Kay <kay.knoepfle@exxeta.com>
2026-01-07 19:42:31 +0100

    fix: correct output value for static web app API token

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,5 +1,5 @@
 output "static_web_app_api_token" {
-  value       = azurerm_static_web_app.frontend_app.api_token
+  value       = azurerm_static_web_app.frontend_app.api_key
   description = "The API token for the static web app."
   sensitive   = true
 }
