e3984f8096eea816fc62c86dd8286dadf71bbde8
Kay <kay.knoepfle@exxeta.com>
2026-01-07 19:11:08 +0100

    feat: add Azure infrastructure for AI playground and outputs for static web app API token
    - Created a new resource group for the AI playground in Germany West Central.
    - Added a cognitive account for LLM with deployment configuration for GPT-5 Mini.
    - Introduced output for the static web app API token, marked as sensitive.

---
+++ infrastructure/ai_playground.tf
@@ -0,0 +1,28 @@
+resource "azurerm_resource_group" "ai_playground_rg" {
+  name     = "${var.prefix}-ai-playground-rg"
+  location = "Germany West Central"
+}
+
+resource "azurerm_cognitive_account" "ai_playground_account_llm" {
+  name                = "${var.prefix}-llm-account-playground"
+  location            = azurerm_resource_group.ai_playground_rg.location
+  resource_group_name = azurerm_resource_group.ai_playground_rg.name
+  kind                = "AIServices"
+  sku_name            = "S0"
+}
+
+resource "azurerm_cognitive_deployment" "ai_playground_deployment_llm" {
+  name                 = "${var.prefix}-llm-deployment-playground"
+  cognitive_account_id = azurerm_cognitive_account.ai_playground_account_llm.id
+
+  sku {
+    name     = "GlobalStandard"
+    capacity = 100
+  }
+
+  model {
+    format  = "OpenAI"
+    name    = "gpt-5-mini"
+    version = "2025-08-07"
+  }
+}
