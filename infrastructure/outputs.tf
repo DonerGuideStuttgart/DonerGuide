2ff48759e5f225f49900968aaac63abee23889b0
Kay <kay.knoepfle@exxeta.com>
2026-01-08 11:01:37 +0100

    feat: enhance frontend workflow to export API mock URL and update environment variable

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -3,3 +3,8 @@ output "static_web_app_api_token" {
   description = "The API token for the static web app."
   sensitive   = true
 }
+
+output "api_mock_url" {
+  value       = azurerm_container_app_environment.api_mock_env.default_domain
+  description = "The default domain of the API mock container app environment."
+}
