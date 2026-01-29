79d52b5eb1255813403cefd4c89031c176555702
anna_rinck <annarinck@gmx.de>
2026-01-29 15:19:44 +0100

    adjust map pin

--- frontend/src/components/Map/StoreMap.tsx
+++ frontend/src/components/Map/StoreMap.tsx
@@ -3,7 +3,7 @@ import { StoreBase } from '@/types/store'
 import { useRef, useState } from 'react'
 import Map, { MapRef, Marker } from 'react-map-gl/mapbox'
 import { StoreMarkerPopup } from './StoreMarkerPopup'
-import Logo from '@/assets/logo/logo.svg'
+import MapPin from '@/assets/logo/map_pin.svg'
 
 interface StoreMapProps {
 	stores: StoreBase[]
@@ -52,19 +52,7 @@ export default function StoreMap({ stores }: StoreMapProps) {
 						}}
 					>
 						<div className="cursor-pointer transform transition-transform hover:scale-110">
-							<Logo height="40" width="32" />
-							{/*<svg
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 29.646 58.804"
-								width="32"
-								height="40"
-							>
-								<path
-									d="M141.694-511.566a1.782,1.782,0,0,0-.46.357c-.219.242-.23.437-.265,3.98l-.035 3.716-5.545.034-5.545.035-.656.322a3.337 3.337 0 0 0-1.9 2.715c-.035.483.759 5.729 3.06 20.246 1.714 10.779 3.152 19.648 3.2 19.729a3.227 3.227 0 0 0 .3.391c.23.253.253.253 3.658.253h3.428l.035 3.163.035 3.164.322.311a1.07 1.07 0 0 0 1.541 0l.322-.311.035-3.164.034-3.163h3.428c3.405 0 3.428 0 3.658-.253a3.9 3.9 0 0 0 .311-.4c.1-.2 6.281-39.239 6.281-39.745a3.714 3.714 0 0 0-2.048-3c-.529-.253-.667-.253-6.085-.288l-5.545-.034-.034-3.727-.035-3.739-.288-.288A1.218 1.218 0 0 0 141.694-511.566Z"
-									transform="translate(-127.291 511.626)"
-									fill="#fb783a"
-								/>
-							</svg>*/}
+							<MapPin height="40" width="20" />
 						</div>
 					</Marker>
 				))}
