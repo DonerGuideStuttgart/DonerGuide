905372d76f3e034ba0d5a2e3ac3006241a5f06a6
anna_rinck <annarinck@gmx.de>
2026-01-29 14:34:43 +0100

    change map pins to logo, fix linting findings

--- frontend/src/components/Map/StoreMap.tsx
+++ frontend/src/components/Map/StoreMap.tsx
@@ -3,6 +3,7 @@ import { StoreBase } from '@/types/store'
 import { useRef, useState } from 'react'
 import Map, { MapRef, Marker } from 'react-map-gl/mapbox'
 import { StoreMarkerPopup } from './StoreMarkerPopup'
+import Logo from '@/assets/logo/logo.svg'
 
 interface StoreMapProps {
 	stores: StoreBase[]
@@ -45,32 +46,25 @@ export default function StoreMap({ stores }: StoreMapProps) {
 						latitude={store.location.coordinates.lat}
 						longitude={store.location.coordinates.lng}
 						anchor="bottom"
-						onClick={(e: {
-							originalEvent: { stopPropagation: () => void }
-						}) => {
+						onClick={(e) => {
 							e.originalEvent.stopPropagation()
 							setSelectedStore(store)
 						}}
 					>
 						<div className="cursor-pointer transform transition-transform hover:scale-110">
-							{/* Custom marker icon matching DaisyUI theme */}
-							<svg
+							<Logo height="40" width="32" />
+							{/*<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 29.646 58.804"
 								width="32"
 								height="40"
-								viewBox="-2 -2 36 44"
-								fill="none"
-								xmlns="http://www.w3.org/2000/svg"
 							>
-								{/* Pin shape */}
 								<path
-									d="M16 0C7.16 0 0 7.16 0 16C0 28 16 40 16 40C16 40 32 28 32 16C32 7.16 24.84 0 16 0Z"
+									d="M141.694-511.566a1.782,1.782,0,0,0-.46.357c-.219.242-.23.437-.265,3.98l-.035 3.716-5.545.034-5.545.035-.656.322a3.337 3.337 0 0 0-1.9 2.715c-.035.483.759 5.729 3.06 20.246 1.714 10.779 3.152 19.648 3.2 19.729a3.227 3.227 0 0 0 .3.391c.23.253.253.253 3.658.253h3.428l.035 3.163.035 3.164.322.311a1.07 1.07 0 0 0 1.541 0l.322-.311.035-3.164.034-3.163h3.428c3.405 0 3.428 0 3.658-.253a3.9 3.9 0 0 0 .311-.4c.1-.2 6.281-39.239 6.281-39.745a3.714 3.714 0 0 0-2.048-3c-.529-.253-.667-.253-6.085-.288l-5.545-.034-.034-3.727-.035-3.739-.288-.288A1.218 1.218 0 0 0 141.694-511.566Z"
+									transform="translate(-127.291 511.626)"
 									fill="#fb783a"
-									stroke="#341c0a"
-									strokeWidth="2"
 								/>
-								{/* Inner circle */}
-								<circle cx="16" cy="16" r="6" fill="#341c0a" />
-							</svg>
+							</svg>*/}
 						</div>
 					</Marker>
 				))}
