b85a6f8eac25b7b9c9612b77d9a6a663a08a9923
Kay <kay.knoepfle@exxeta.com>
2026-02-01 09:56:04 +0100

    add hello world http trigger and upgraded build scripts

---
+++ backend/functions/shops/src/functions/ping_pong.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+
+app.http("ping_pong", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  route: "ping",
+  handler: ping_pong,
+});
+
+export function ping_pong(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+  return {
+    status: 200,
+    body: "pong",
+  };
+}
