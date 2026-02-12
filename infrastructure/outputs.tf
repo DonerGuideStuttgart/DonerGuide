3b28fc26aae0a3fcd7f932b633c5de58e2bafd35
Kay <kay.knoepfle@exxeta.com>
2026-02-12 22:35:34 +0100

    feat: Use the actual API URL from Terraform output in the frontend build workflow instead of the mock URL.

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -9,6 +9,11 @@ output "api_mock_url" {
   description = "The default domain of the API mock container app environment."
 }
 
+output "api_url" {
+  value       = "https://${azurerm_app_service_custom_hostname_binding.shops_function_hostname.hostname}"
+  description = "The default domain of the API container app environment."
+}
+
 output "function_app_name_place_search" {
   value       = azurerm_linux_function_app.place-search-function.name
   description = "The name of the place search function app."
