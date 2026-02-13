6132348d61061ee6fecf0a027df69775e11f6d81
Levartar <jakob.guenster@gmx.net>
2026-02-13 18:04:25 +0200

    fix: try to prefetch all stores

--- frontend/src/app/(entdecken)/[slug]/page.tsx
+++ frontend/src/app/(entdecken)/[slug]/page.tsx
@@ -61,7 +61,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 }
 
 export async function generateStaticParams() {
-	const data = await fetchPlaces('')
+	const data = await fetchPlaces('limit=1000') // Fetch all places
 	const places = data.items || []
 
 	return places.map((place: StoreBase) => ({
