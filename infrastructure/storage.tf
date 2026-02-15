2d41a38c36f22291dc37113b1cb0708b5e783dcf
Kay <kay.knoepfle@exxeta.com>
2026-02-15 10:06:58 +0100

    feat: Configure public access settings for the generated images storage account and container.

--- infrastructure/storage.tf
+++ infrastructure/storage.tf
@@ -14,18 +14,20 @@ resource "azurerm_storage_container" "sc_classified_images" {
 }
 
 resource "azurerm_storage_account" "storage_account_generated_images" {
-  name                     = "storagegeneratedimages"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  local_user_enabled       = false
+  name                            = "storagegeneratedimages"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  local_user_enabled              = false
+  allow_nested_items_to_be_public = true
+  public_network_access_enabled   = true
 }
 
 resource "azurerm_storage_container" "sc_generated_images" {
   name                  = "generatedimages"
   storage_account_id    = azurerm_storage_account.storage_account_generated_images.id
-  container_access_type = "public"
+  container_access_type = "blob"
 }
 
 resource "azurerm_storage_account" "storage_account_functions" {
